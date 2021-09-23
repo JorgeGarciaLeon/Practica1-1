3b1699470f1d25544d6b6eabd3f8c4e1af1bd555
JorgeGarciaLeon <78152033+JorgeGarciaLeon@users.noreply.github.com>
2021-09-23 22:56:16 +0100

    Añadidas Notas
    Feature: Se han añadido las notas y sus medias en un ArrayList bidimensional.

--- src/Alumno.java
+++ src/Alumno.java
@@ -6,7 +6,7 @@ public class Alumno {
     //Variables de la clase
     ArrayList<String> NombreAlumnosAleatorio = new ArrayList<>();
     ArrayList<String> NombreAlumnosCompleto = new ArrayList<>();
-    ArrayList<Double>[][] NotasAlumnos = new ArrayList[0][0];
+    ArrayList<ArrayList<Double>> NotasAlumnos = new ArrayList<>();
 
     //Method
     public void MostrarNotasAlumnos(){
@@ -32,7 +32,25 @@ public class Alumno {
                 NombreAlumnosAleatorio.add(nombreAlu);
             }
 
-           
+
+            //Añadimos las 3 notas y la media de ambas
+            double NotaMedia = 0;
+            double nota = 0;
+
+            for (int j = 0; j < 10; j++) {
+                NotasAlumnos.add(new ArrayList<>());
+                for (int k = 1; k <= 4; k++) {
+                    if(k!=3){
+                        nota = Math.random()*10;
+                        NotasAlumnos.get(j).add(nota);
+                        NotaMedia += nota;
+                    }else{
+                        NotaMedia = NotaMedia/3;
+                        NotasAlumnos.get(j).add(NotaMedia);
+                    }
+                }
+            }
+
 
 
 
